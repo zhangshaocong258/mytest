37dfd92baca9ea33e2aa087a69c9d834b5d6654b
zhangshaocong258 <597001494@qq.com>
2016-12-14 15:28:54 +0800

    索引过滤

--- src/main/java/com/szl/HanLPTest.java
+++ src/main/java/com/szl/HanLPTest.java
@@ -1,19 +1,43 @@
 package com.szl;
 
 import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.dictionary.CustomDictionary;
+import com.hankcs.hanlp.dictionary.other.CharType;
+import com.hankcs.hanlp.dictionary.stopword.CoreStopWordDictionary;
+import com.hankcs.hanlp.dictionary.stopword.Filter;
 import com.hankcs.hanlp.seg.NShort.NShortSegment;
 import com.hankcs.hanlp.seg.common.Term;
+import com.hankcs.hanlp.tokenizer.BasicTokenizer;
 import com.hankcs.hanlp.tokenizer.IndexTokenizer;
+import com.hankcs.hanlp.tokenizer.NotionalTokenizer;
 import com.hankcs.hanlp.tokenizer.StandardTokenizer;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by zsc on 2016/12/12.
+ * CustomDictionary中添加新词时，需要删除下面的bin，其他的应该类似，间隔是空格
+ * filter用于索引分词的去停用词，只能自己定义，没有api
+ *
  */
 public class HanLPTest {
     public static void main(String args[]) {
-//        System.out.println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
+        CoreStopWordDictionary.FILTER = new Filter()
+        {
+            @Override
+            public boolean shouldInclude(Term term)
+            {
+                switch (term.nature)
+                {
+                    case nz:
+                        return !CoreStopWordDictionary.contains(term.word);
+                }
+                return false;
+            }
+        };
+        System.out.println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
         System.out.println(StandardTokenizer.segment("这样的人才能经受住考验"));
         System.out.println(StandardTokenizer.segment("在这些企业中国有企业有十个"));
         System.out.println(StandardTokenizer.segment("原子结合成分子时"));
@@ -51,9 +75,81 @@ public class HanLPTest {
         System.out.println(HanLP.segment("北京大学生前来应聘"));
         System.out.println(HanLP.segment("结婚的和尚未结婚的"));
         System.out.println(HanLP.segment("这事的确定不下来"));
+        System.out.println(HanLP.segment("有关刘晓庆偷税案"));
+        System.out.println(HanLP.segment("大学生活动中心"));
+        System.out.println(HanLP.segment("有意见分歧"));
+        System.out.println(HanLP.segment("南京市长江大桥"));
 
+//        CustomDictionary.insert("白富美", "nz 1024");
+        System.out.println(IndexTokenizer.segment("为何赵本山的徒弟比郭德纲的徒弟忠诚度要高？"));//要去掉问号
+        System.out.println(StandardTokenizer.segment("张少聪-知乎"));//要去掉问号
+        System.out.println(StandardTokenizer.segment("不要不留后路"));//要去掉问号
+        System.out.println(IndexTokenizer.segment("《神奇动物在哪里》中有哪些不易察觉的彩蛋或细节？"));
+        System.out.println(IndexTokenizer.segment("在阿里巴巴当hr是一种怎样的体验？"));
+        System.out.println(filter(IndexTokenizer.segment("在阿里巴巴当hr是一种怎样的体验？")));
+        System.out.println(filter(IndexTokenizer.segment("《神奇动物在哪里》中有哪些不易察觉的彩蛋或细节？？")));
+        System.out.println(IndexTokenizer.segment("郑州副局长要火"));
+        System.out.println(filter(IndexTokenizer.segment("副局长要火")));
+        System.out.println(filter(IndexTokenizer.segment("张少聪-知乎")));
+        System.out.println(HanLP.extractKeyword("为何赵本山的徒弟比郭德纲的徒弟忠诚度要高", 20));
+        System.out.println(HanLP.extractKeyword("郑州副局长要火", 20));
+        System.out.println(HanLP.extractKeyword("在阿里巴巴当hr是一种怎样的体验", 20));
+        System.out.println(HanLP.extractKeyword("《神奇动物在哪里》中有哪些不易察觉的彩蛋或细节", 20));
+        System.out.println(HanLP.extractKeyword("哈利·波特如果当时进了斯莱特林学院会怎么样", 20));
+        System.out.println(NotionalTokenizer.segment("为何赵本山的徒弟比郭德纲的徒弟忠诚度要高"));
+        System.out.println(NotionalTokenizer.segment("在阿里巴巴当hr是一种怎样的体验"));
+        System.out.println(NotionalTokenizer.segment("《神奇动物在哪里》中有哪些不易察觉的彩蛋或细节"));
+        System.out.println(NotionalTokenizer.segment("哈利·波特如果当时进了斯莱特林学院会怎么样"));
+        System.out.println(NotionalTokenizer.segment("三个臭皮匠"));
+        System.out.println(NotionalTokenizer.segment("怎样在大学宿舍学习"));
+        System.out.println(NotionalTokenizer.segment("郑州副局长要火"));
+        System.out.println(NotionalTokenizer.segment("在阿里巴巴当hr是一种怎样的体验"));
+        System.out.println(StandardTokenizer.segment("郑州副局长要火"));
+    }
 
-        System.out.println(IndexTokenizer.segment("在阿里巴巴 当 HR 是怎样一种体验"));
-        System.out.println(HanLP.extractKeyword("在阿里巴巴当HR是怎样一种体验", 10));
+    private static List<Term> filter(List<Term> termList) {
+        List<Term> temp = new ArrayList<Term>();
+        for (int i = 0; i < termList.size(); i++) {
+            Term term = termList.get(i);
+            String nature = term.nature != null ? term.nature.toString() : "空";
+            char firstChar = nature.charAt(0);
+            switch(firstChar) {
+                case 'b': //区别词 正 副
+                case 'z': //状态词
+                case 'r': //代词 怎样 如何
+                    break;
+                case 'c':
+                case 'e':
+                case 'm':
+                case 'o':
+                case 'p':
+                case 'q':
+
+                case 'u':
+                case 'w':
+                case 'y':
+                    temp.add(term);
+                    break;
+                case 'd':
+                case 'f':
+                case 'g':
+                case 'h':
+                case 'i':
+                case 'j':
+                case 'k':
+                case 'l':
+                case 'n':
+                case 's':
+                case 't':
+                case 'v':
+                case 'x':
+                default:
+                    if(term.word.length() == 1) {//长度为1，删除，可以理解为没有分出来词，因此删除，最后查询时分出的词，也可以删除停用词
+                        temp.add(term);
+                    }
+            }
+        }
+        termList.removeAll(temp);
+        return termList;
     }
 }
