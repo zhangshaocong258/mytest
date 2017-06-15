eed2a4b97401ccc41a24f22747914202545d0830
zhangshaocong258 <597001494@qq.com>
2017-06-15 20:22:13 +0800

    redis 序列化 反序列化

--- src/main/java/com/szl/JedisTest.java
+++ src/main/java/com/szl/JedisTest.java
@@ -2,21 +2,61 @@ package com.szl;
 
 import redis.clients.jedis.Jedis;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by zsc on 2016/12/1.
  */
 public class JedisTest {
     public static void main(String args[]) {
         Jedis jedis = new Jedis("localhost", 6379);
-        jedis.sadd("user","liuling2");
-        jedis.sadd("user","xinxin2");
-        jedis.sadd("user","ling2");
-        jedis.sadd("user","zhangxinxin2");
-        jedis.sadd("user","who2");
+
+//        Set<String> set = jedis.smembers("visitedUrl");
+//        HashSet<String> hashSet = new HashSet<>(set);
+//        saveObj(hashSet);
+//        HashSet<String> hashSet2 = (HashSet<String>)getObj();
+//        for (String str : hashSet2) {
+//            jedis.sadd("visitedUrl", str);
+//        }
         jedis.save();
 
-        System.out.println(jedis.smembers("user"));
-        System.out.println(jedis.sismember("user", "xinxin"));//判断 who 是否是user集合的元素
+    }
 
+    private static void saveObj(Object object) throws IOException{
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream(new File("E:\\set")));
+            oos.writeObject(object);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("write model error");
+        } finally {
+            if (oos != null) {
+                oos.close();
+            }
+        }
+    }
+
+    private static Object getObj() {
+        Object object = null;
+        try {
+            ObjectInputStream ois = null;
+            try {
+                ois = new ObjectInputStream(new FileInputStream(new File("E:\\set")));
+                object = ois.readObject();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (ois != null) {
+                    ois.close();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return object;
     }
 }
