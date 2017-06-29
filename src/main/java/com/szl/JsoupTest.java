b8a5c04a14b9bd6c865616e3c8d5c7c16c3b92e3
zhangshaocong258 <597001494@qq.com>
2017-06-29 16:35:57 +0800

    jsoup select

--- src/main/java/com/szl/JsoupTest.java
+++ src/main/java/com/szl/JsoupTest.java
@@ -14,7 +14,7 @@ import java.io.*;
  */
 public class JsoupTest {
     public static void main(String[] args) {
-        File file = new File("src\\main\\resources\\zhihu.html");//地址路径，相对是src同级路径
+        File file = new File("src\\main\\resources\\www.zhihu.com_question_27424671.html");//地址路径，相对是src同级路径
         InputStreamReader reader = null;
         StringWriter writer = new StringWriter();
         try {
@@ -39,17 +39,21 @@ public class JsoupTest {
         if (writer != null) {
             String html = writer.toString();
             String baseUrl = "https://www.zhihu.com";
-            Document document = Jsoup.parse(html, baseUrl);
-            Elements links = document.select("script");
-            for (int i = 0; i < links.size(); i++) {
-                if (links.get(i).attr("data-reactid").equals("21")) {
-                    links.get(i).remove();
-                }
+            Document doc = Jsoup.parse(html, baseUrl);
+//            Elements links = document.select("script");
+//            for (int i = 0; i < links.size(); i++) {
+//                if (links.get(i).attr("data-reactid").equals("21")) {
+//                    links.get(i).remove();
+//                }
 //                links.get(i).removeAttr("src");
 //                System.out.println("script: " + links.get(i).toString());
 //                System.out.println("script: " + links.get(i).attr("src"));
-            }
+            //www.zhihu.com_question_27424671.html 当div中class有多个时select多次
+            System.out.println(doc.select("div.zm-editable-content").select("div.clearfix").text());
+            String description = doc.select("div.zm-item-vote-info").first().attr("data-votecount") + ",," +
+                    doc.select("div.zm-item-answer-author-info").first().select("a.author-link").text() + ",," +
+                    doc.select("div.RichContent-inner").first().text().substring(0, doc.select("div.RichContent-inner").first().text().length()
+                            > 300 ? 300 : doc.select("div.RichContent-inner").first().text().length()) + "...";
         }
-
     }
 }
