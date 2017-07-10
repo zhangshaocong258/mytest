5a447f805bdb05395e83f42df9a37d6aa8c3ccff
zhangshaocong258 <597001494@qq.com>
2017-07-10 23:03:26 +0800

    static初始化

--- src/main/java/com/szl/StaticTest.java
+++ src/main/java/com/szl/StaticTest.java
@@ -3,6 +3,8 @@ package com.szl;
 /**
  * Created by zsc on 2016/10/21.
  * 静态初始化，调用时初始化
+ * 若1有static关键字，那么在执行2时，k被赋值为5，所以输出5
+ * 若1没有static关键字，那么在执行2时，k为0
  */
 public class StaticTest {
     static int j = getI();
@@ -11,8 +13,9 @@ public class StaticTest {
         return i;
     }
     static int k;
-    static StaticTest staticTest= new StaticTest();//添加或不添加static
+    static StaticTest staticTest= new StaticTest();//1 添加或不添加static
     public StaticTest() {
+        System.out.println("构造器" + k);
         k = 5;
     }
 
