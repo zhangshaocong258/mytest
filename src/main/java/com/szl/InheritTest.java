9cc83c853c1b385e703dbdeaf9824a89e3f17934
zhangshaocong258 <597001494@qq.com>
2017-10-26 21:37:28 +0800

    继承

---
+++ src/main/java/com/szl/InheritTest.java
@@ -0,0 +1,22 @@
+package com.szl;
+
+/**
+ * Created by zsc on 2017/9/15.
+ */
+public class InheritTest {
+    public static void main(String args[]) {
+
+    }
+}
+
+class A{
+    public void add(){
+
+    }
+}
+
+class B extends A {
+    protected void add(String str) {
+
+    }
+}
